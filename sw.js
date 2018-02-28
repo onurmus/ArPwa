02a7508cd2240cbbc7b721f1336afd00770d0756
onurmus <onur.musaoglu@arcelik.com>
2018-02-28 15:26:10 +0300

    notification removed

--- sw.js
+++ sw.js
@@ -27,53 +27,6 @@
 
 /* eslint-enable max-len */
 
-function urlB64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
-
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
-self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push Received.');
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-
-  const title = 'Ar PWA';
-  const options = {
-    body: event.data.text(),
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: 1
-    },
-    icon: 'images/frontend/favicon.ico',
-    badge: 'images/badge.png',
-    actions: [
-    { "action": "yes", "title": "Yes", "icon": "images/yes.png" },
-    { "action": "no", "title": "No", "icon": "images/no.png" }
-  ]
-  };
-
-  event.waitUntil(self.registration.showNotification(title, options));
-});
-
-self.addEventListener('notificationclick', function(event) {
-  console.log('[Service Worker] Notification click Received.');
-
-  event.notification.close();
-
-  event.waitUntil(
-    clients.openWindow('https://onurmus.github.io/FirstProgressiveWebApp/')
-  );
-});
 
 self.addEventListener('fetch', function(event){
 
